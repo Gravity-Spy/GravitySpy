aa324faeb63b013917e4a86a1581ff825980109b
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2016-06-14 08:26:25 -0500

    Cleaning up master after merge of Branches

--- sync.sh
+++
@@ -1,1 +0,0 @@
-rsync -avz -e ssh --chmod=g-s --include="*/" --include="**/*.tar" --include="**/*.txt" --include="**/*.csv" --exclude="**/*.png" /home/michael.zevin/public_html/GravitySpy/ sbc538@quest.it.northwestern.edu:/xvaultevfs1/b1011/GravitySpy/H1/
