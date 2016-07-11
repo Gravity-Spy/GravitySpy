a0b5546b0c7ed8f61ac269496fae2ae48c46b95f
Scott Coughlin <scott.coughlin@ligo.org>
2016-07-11 15:02:14 -0500

    Finish change to pandas and finished the decider function

--- ML/ML_py/gravspy_main2.py
+++ ML/ML_py/gravspy_main2.py
@@ -37,11 +37,10 @@ retired_images = pd.DataFrame({ 'imageID' : tmpRI})
 PP_matrices    = pd.DataFrame({ 'imageID' : tmpPP,'pp_matrix' : tmpPP1}) 
 
 #decider function to determine where an image is placed
-def decider(pp_matrix, ML_dec, t, R_lim, no_annotators):
+def decider(pp_matrix, ML_dec, t, R_lim, num_annotators):
 
-    pp_matrix2 = np.append(pp_matrix, ML_dec.reshape((15,1))) #concatenate transpose of ML_decision to pp_matrix
-    set_trace()
-    v = np.sum(pp_matrix2, axis=1)/np.sum(pp_matrix) #create vector of normalized sums of pp_matrix2
+    pp_matrix2 = np.hstack((pp_matrix, ML_dec.reshape((15,1)))) #concatenate transpose of ML_decision to pp_matrix
+    v = np.sum(pp_matrix2, axis=1)/np.sum(np.sum(pp_matrix2)) #create vector of normalized sums of pp_matrix2
     maximum = np.amax(v) #initialize maximum, max value of v
     maxIdx = np.argmax(v) #initialize maxIdx, index of max value of v
 
@@ -50,7 +49,7 @@ def decider(pp_matrix, ML_dec, t, R_lim, no_annotators):
         decision = 1
         print('Image is retired')
 
-    elif no_annotators >= R_lim: #if more than R_lim annotators have looked at image and no decision reached, pass to more experience users
+    elif num_annotators >= R_lim: #if more than R_lim annotators have looked at image and no decision reached, pass to more experience users
 
         decision = 2
         print('Image is given to the upper class')
@@ -139,8 +138,7 @@ def main_trainingandtest(images,conf_matrices,PP_matrices):
                 for y in range(len(PP_matrices)): #iterate over posterior matrices
 
                     if imageID == PP_matrices['imageID'][y]: #find posterior matrix for the image
-
-                        image_prior = np.sum(PP_matrices['matrix'][y],axis=1)/np.sum(PP_matrices['matrix'][y]) #if image labeled but not retired, PP_matrix information is used in the place of priors
+                        image_prior = np.sum(PP_matrices['pp_matrix'][y],axis=1)/np.sum(PP_matrices['pp_matrix'][y]) #if image labeled but not retired, PP_matrix information is used in the place of priors
                         break
 
                 for k in range(num_annotators): #iterate over citizens that labeled image
@@ -148,44 +146,118 @@ def main_trainingandtest(images,conf_matrices,PP_matrices):
 
                         if userIDs[k] == conf_matrices['userID'][iN]: #find confusion matrix corresponding to citizen
 
-                            conf = conf_matrices['matrix'][iN] #take confusion matrix of citizen
+                            conf = conf_matrices['conf_matrix'][iN] #take confusion matrix of citizen
                             break
 
                     conf_divided,x,z,s = np.linalg.lstsq(np.diag(sum(conf,2)),conf) #calculate p(l|j) value
 
                     for j in range(C): #iterate over classes
 
-                        pp_matrix = np.zeros((C,no_annotators)) #create posterior matrix
+                        pp_matrix = np.zeros((C,num_annotators)) #create posterior matrix
                         pp_matrix[j,k] = (conf_divided[j,labels[k]]*priors[0][j])/sum(conf_divided[:,labels[k]]*priors[0]) #calculate posteriors
                 pp_matrices_rack.append(pp_matrix) #assign values to pp_matrices_rack
 
 
-                dec_matrix[0,i], class_matrix[0,i] = decider(pp_matrix, ML_dec, t, R_lim, no_annotators) #make decisions for each image in batch
+                dec_matrix[0,i], class_matrix[0,i] = decider(pp_matrix, ML_dec, t, R_lim, num_annotators) #make decisions for each image in batch
 
-"""for jj in range(0, len(data['conf_matrices']): #for all citizens
-	conf_update = data['conf_matrices'][IDs[jj-1]-1][0] #confusion matrices taken one by one
-	conf_update_divided = np.linalg.sovle(np.diag(np.sum(conf_update, axis=0))), conf_update)
-	alpha[:,jj] = np.diag(conf_update_divided)
-	###TEST CODE###"""
+"""
+At this point, the decisions for each image in the batch are given. For
+golden images in the set, the decision is 0. For the ML labelled images, the
+decisions are one of 1,2, or 3.
 
-"""#update the confusion matrices for test batch and promotion
-  for i in range(0,N):
-    if dec_matrix[0,i] == 1: #if image is retired
-      labels = batch['images'][i]['labels'][0][0] #the citizen label of the image is taken
-      IDs = batch['images'][i]['IDs'][0][0] #the IDs of the citizens that labeld that image are taken
-      for ii in (0, len(IDs)): #for each citizen
-        conf_matrix = batch['conf_matrices'][IDs[ii-1]-1][0] #take confusion matrix of each citizen
-        conf_matrix[class_[i]-1,labels[ii]-1] = conf_matrix[class_[i]-1,labels[ii]-1]+1 #update confusion matrix
-        batch['conf_matrices'][IDs[ii]-1][0] = conf_matrix"""
+The posterior probability matrices are kept for all the ML labelled images. If
+the decision is 2 or 3, the probabilities in this matrix will be used in a
+further step. Not currently implemented.
 
-"""for jj in range(0, len(batch['conf_matrices']): #for all citizens
-	conf_update = batch['conf_matrices'][IDs[jj-1]-1][0] #confusion matrices taken one by one
-	#### FINISH ####"""
+Also, the confusion matrices are updated based on the golden images.
 
-"""counter1 = len(data['images'][i]['PP_matrices'][0][0] + 1
-counter2 = len() + 1
-for i in range(0,N):
-	if de"""
+Next step is updating the confusion matrices for the test images and
+citizen evaluation/promotion.
+
+
+Updating the Confusion Matrices for Test Data and Promotion
+"""
+    for i in range(N):
+        if dec_matrix[0,i] == 1: #if image is retired
+            labels = images['labels'][i] #the citizen label of the image is taken
+            userIDs = images['userIDs'][i] #the IDs of the citizens that labeld that image are taken
+            for ii in range(userIDs.size): #iterate over user IDs of image
+
+                indicator2 = 0
+
+                for cc in range(len(conf_matrices)): #iterate over confusion matrices
+
+                    if userIDs[ii] == conf_matrices['userID'][cc]: #if user is registered
+
+                        conf_matrix = conf_matrices['conf_matrix'][cc] #take confusion matrix of citizen
+                        conf_matrix[tlabel,labels[ii]] += 1 #update confusion matrix
+                        conf_matrices['conf_matrix'][cc] = conf_matrix #confusion matrix put back in stack
+                        indicator2 = 1
+
+                if indicator2 == 0: #if user not registered
+
+                    dummy_matrix = np.zeros((C,C)) #create dummy matrix
+                    dummy_matrix[tlabel,labels[ii]] += 1 #update dummy matrix
+                    tmp = pd.DataFrame({ 'userID' : [userIDs[ii]],'conf_matrix' : [dummy_matrix]},index = [len(conf_matrices)])
+                    conf_matrices = conf_matrices.append(tmp)
+
+    for jj = 1:len(conf_matrices)  # for all the citizens
+    
+        conf_update = conf_matrices['conf_matrix'][jj]; # their conf. matrices are taken one by one
+    
+        conf_update_divided,x,z,s = np.linalg.lstsq(np.diag(sum(conf_update,2)),conf_update) #calculate p(l|j) value 
+    
+        alpha[:,jj] = np.diag(conf_update_divided);    # alpha parameters are recalculated
+
+#Thresholding alpha vectors and citizen evaluation (needs work)
+
+    
+# Ordering the images and sending/saving them
+
+counter1 = length(retired_images) + 1;
+counter2 = length(PP_matrices) + 1;
+
+for i = 1:N %for each image
+    
+    if decision(i) == 1     %if it is decided to be retired
+        
+        retired_images(counter1).imageID = images(i).imageID;         %it is put into the retired images array with the ID and the class it is classified into.
+        retired_images(counter1).class = class(i);
+        
+        for y = 1:length(PP_matrices)  %in case the retired image was waiting for more labels beforehand
+            
+            if images(i).imageID == PP_matrices(y).imageID        
+                
+                PP_matrices(y) = [];       %the PP matrix is taken out of the saved matrices.
+                break
+            end
+        end
+        
+        counter1 = counter1 + 1;
+    
+    elseif decision(i) == 2 || decision(i) == 3      %if the decision is forwarding to the upper class or wait for more labels
+        
+        dummy_decider = 1;
+        
+        for y = 1:length(PP_matrices)        %in case the image was waiting for more labels beforehand
+            
+            if images(i).imageID == PP_matrices(y).imageID
+                PP_matrices(y).imageID = images(i).imageID;      %the PP matrix is overwritten.
+                dummy_decider = 0;
+                break
+            end
+        end
+        
+        if dummy_decider
+        
+            PP_matrices(counter2).imageID = images(i).imageID;           %The PP matrix of the image is saved with the corresponding ID to be used in the place of the prior in the next batch
+            PP_matrices(counter2).matrix = pp_matrices_rack{i};
+            counter2 = counter2 + 1;
+        end
+        
+        
+    end
+end"""
 
 #for loop to iterate over each batch
 for i in range(1,2):
