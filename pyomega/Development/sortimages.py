15d3da00cd8adbd908297e8dc14645dc1bf996b5
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2016-11-17 12:40:47 -0600

    Ready to add images to website using the python client

--- pyomega/Development/sortimages.py
+++ pyomega/Development/sortimages.py
@@ -33,49 +33,49 @@ M  = 2117
 
 if opts.detector == 'H1':
     workflow_subject_set_dict_app = {
-"Light_Modulation":((A,5653,[1,0.9]),(M,5654,0.9)),
-"Air_Compressor":((A,5651,[1,0.6]),(M,5652,0.6)),
-"Blip":((B1,5655,.998),(A,5656,[.998,.85]),(M,5657,.85)),
-"Chirp":((B3,5658,1),(A,5659,[1,.5]),(M,5660,.50)),
-"Extremely_Loud":((A,5661,[1,.815]),(M,5662,.815)),
-"Helix":((A,5663,[1,.50]),(M,5664,.50)),
-"Koi_Fish":((B2,5665,.98),(A,5666,[.98,.621]),(M,5667,.621)),
-"Low_Frequency_Burst":((B3,5905,.99995),(A,5668,[.99995,.93]),(M,5669,.93)),
-"Low_Frequency_Lines":((A,5670,[1,.65]),(M,5671,.65)),
-"No_Glitch":((B3,5672,.9901),(A,5673,[.9901,.85]),(M,5674,.85)),
-"None_of_the_Above":((A,5676,[1,.50]),(M,5677,.50)),
-"Paired_Doves":((A,5678,[1,.5]),(M,5679,.50)),
-"Power_Line":((B2,5682,.998),(A,5680,[.998,.86]),(M,5681,.86)),
-"Repeating_Blips":((A,5683,[1,.686]),(M,5684,.686)),
-"Scattered_Light":((B3,5685,.99965),(A,5686,[.99965,.96]),(M,5687,.96)),
-"Scratchy":((A,5688,[1,.913]),(M,5689,.913)),
-"Tomte":((A,5690,[1,.7]),(M,5691,.7)),
-"Violin_Mode":((B2,5904,1),(A,5692,[1,.5]),(M,5693,.50)),
-"Wandering_Line":((A,5694,[1,.97]),(M,5695,.97)),
-"Whistle":((B1,5696,1),(A,5697,[1,.6]),(M,5698,.6)),
+"Air_Compressor":((A,6714,[1,0.6]),(M,6715,0.6)),
+"Blip":((B1,6717,.998),(A,6718,[.998,.85]),(M,6719,.85)),
+"Chirp":((B3,6721,1),(A,6722,[1,.5]),(M,6723,.50)),
+"Extremely_Loud":((A,6725,[1,.815]),(M,6726,.815)),
+"Helix":((A,6728,[1,.50]),(M,6729,.50)),
+"Koi_Fish":((B2,6731,.98),(A,6732,[.98,.621]),(M,6733,.621)),
+"Light_Modulation":((A,6752,[1,0.9]),(M,6753,0.9)),
+"Low_Frequency_Burst":((B3,6755,.99995),(A,6756,[.99995,.93]),(M,6757,.93)),
+"Low_Frequency_Lines":((A,6759,[1,.65]),(M,6760,.65)),
+"No_Glitch":((B3,6762,.9901),(A,6763,[.9901,.85]),(M,6764,.85)),
+"None_of_the_Above":((A,6766,[1,.50]),(M,6767,.50)),
+"Paired_Doves":((A,6769,[1,.5]),(M,6770,.50)),
+"Power_Line":((B2,6772,.998),(A,6773,[.998,.86]),(M,6774,.86)),
+"Repeating_Blips":((A,6776,[1,.686]),(M,6777,.686)),
+"Scattered_Light":((B3,6779,.99965),(A,6780,[.99965,.96]),(M,6781,.96)),
+"Scratchy":((A,6783,[1,.913]),(M,6784,.913)),
+"Tomte":((A,6786,[1,.7]),(M,6787,.7)),
+"Violin_Mode":((B2,6902,1),(A,6789,[1,.5]),(M,6790,.50)),
+"Wandering_Line":((A,6792,[1,.97]),(M,6793,.97)),
+"Whistle":((B1,6795,1),(A,6796,[1,.6]),(M,6797,.6)),
     }
 elif opts.detector == 'L1':
     workflow_subject_set_dict_app = {
-"Light_Modulation":((A,5653,[1,0.9]),(M,5654,0.9)),
-"Air_Compressor":((A,5651,[1,0.6]),(M,5652,0.6)),
-"Blip":((B1,5655,.998),(A,5656,[.998,.85]),(M,5657,.85)),
-"Chirp":((B3,5658,1),(A,5659,[1,.5]),(M,5660,.50)),
-"Extremely_Loud":((A,5661,[1,.815]),(M,5662,.815)),
-"Helix":((A,5663,[1,.50]),(M,5664,.50)),
-"Koi_Fish":((B2,5665,.98),(A,5666,[.98,.621]),(M,5667,.621)),
-"Low_Frequency_Burst":((B3,5905,.99995),(A,5668,[.99995,.93]),(M,5669,.93)),
-"Low_Frequency_Lines":((A,5670,[1,.65]),(M,5671,.65)),
-"No_Glitch":((B3,5672,.9901),(A,5673,[.9901,.85]),(M,5674,.85)),
-"None_of_the_Above":((A,5676,[1,.50]),(M,5677,.50)),
-"Paired_Doves":((A,5678,[1,.5]),(M,5679,.50)),
-"Power_Line":((B2,5682,.998),(A,5680,[.998,.86]),(M,5681,.86)),
-"Repeating_Blips":((A,5683,[1,.686]),(M,5684,.686)),
-"Scattered_Light":((B3,5685,.99965),(A,5686,[.99965,.96]),(M,5687,.96)),
-"Scratchy":((A,5688,[1,.913]),(M,5689,.913)),
-"Tomte":((A,5690,[1,.7]),(M,5691,.7)),
-"Violin_Mode":((B2,5904,1),(A,5692,[1,.5]),(M,5693,.50)),
-"Wandering_Line":((A,5694,[1,.97]),(M,5695,.97)),
-"Whistle":((B1,5696,1),(A,5697,[1,.6]),(M,5698,.6)),
+"Air_Compressor":((A,6714,[1,0.6]),(M,6715,0.6)),
+"Blip":((B1,6717,.998),(A,6718,[.998,.85]),(M,6719,.85)),
+"Chirp":((B3,6721,1),(A,6722,[1,.5]),(M,6723,.50)),
+"Extremely_Loud":((A,6725,[1,.815]),(M,6726,.815)),
+"Helix":((A,6728,[1,.50]),(M,6729,.50)),
+"Koi_Fish":((B2,6731,.98),(A,6732,[.98,.621]),(M,6733,.621)),
+"Light_Modulation":((A,6752,[1,0.9]),(M,6753,0.9)),
+"Low_Frequency_Burst":((B3,6755,.99995),(A,6756,[.99995,.93]),(M,6757,.93)),
+"Low_Frequency_Lines":((A,6759,[1,.65]),(M,6760,.65)),
+"No_Glitch":((B3,6762,.9901),(A,6763,[.9901,.85]),(M,6764,.85)),
+"None_of_the_Above":((A,6766,[1,.50]),(M,6767,.50)),
+"Paired_Doves":((A,6769,[1,.5]),(M,6770,.50)),
+"Power_Line":((B2,6772,.998),(A,6773,[.998,.86]),(M,6774,.86)),
+"Repeating_Blips":((A,6776,[1,.686]),(M,6777,.686)),
+"Scattered_Light":((B3,6779,.99965),(A,6780,[.99965,.96]),(M,6781,.96)),
+"Scratchy":((A,6783,[1,.913]),(M,6784,.913)),
+"Tomte":((A,6786,[1,.7]),(M,6787,.7)),
+"Violin_Mode":((B2,6902,1),(A,6789,[1,.5]),(M,6790,.50)),
+"Wandering_Line":((A,6792,[1,.97]),(M,6793,.97)),
+"Whistle":((B1,6795,1),(A,6796,[1,.6]),(M,6797,.6)),
     }
 else:
     ValueError("Please select detector you are rewriting the csv file for")
@@ -96,6 +96,24 @@ for Type in types:
         reader = csv.reader(open('{0}/{1}/Beginner/imagemeta.csv'.format(dataPath,Type)), delimiter=",")
         hold = sorted(reader)
         for iX in hold:
+            if not len(ast.literal_eval(iX[6])) == 20:
+                print(ast.literal_eval(iX[6]))
+                ValueError('Problem with CSV file for type: {0}'.format(Type))
+    except:
+        print("Fail: {0}".format(Type))
+
+
+for Type in types:
+    Begin  = []
+    Appre  = []
+    Master = []
+    try:
+        work_subject_dict = workflow_subject_set_dict_app[Type]
+        iN                = indexDict[Type]
+        reader = csv.reader(open('{0}/{1}/Beginner/imagemeta.csv'.format(dataPath,Type)), delimiter=",")
+        hold = sorted(reader)
+
+        for iX in hold:
 
             for combos in work_subject_dict:
                 workflow   = combos[0]
@@ -110,67 +128,65 @@ for Type in types:
                 if (workflow in [M]) and (thres > float(ast.literal_eval(iX[6])[iN])):
                     Master.append(iX)
 
-            for combos in work_subject_dict:
-                workflow   = combos[0]
-                subjectset = SubjectSet.find(6584) #6584 #combos[1]
-                tmp        = []
-                if (workflow in [B1,B2,B3]):
-                    for iXX in Begin:
-                        subject = Subject()
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[2]))
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[3]))
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[4]))
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[5]))
-                        subject.metadata['date']          = iXX[0]
-                        subject.metadata['subject_id']    = iXX[1]
-                        subject.metadata['Filename1']     = iXX[2]
-                        subject.metadata['Filename2']     = iXX[3]
-                        subject.metadata['Filename3']     = iXX[4]
-                        subject.metadata['Filename4']     = iXX[5]
-                        subject.metadata['#ML_Posterior'] = iXX[6]
-                        subject.save()
-                        tmp.append(subject)
-                    subjectset.add(tmp)
+        for combos in work_subject_dict:
+            workflow   = combos[0]
+            subjectset = SubjectSet.find(combos[1])
+            tmp        = []
+            if (workflow in [B1,B2,B3]):
+                for iXX in Begin:
+                    subject = Subject()
+                    subject.links.project = project
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[2]))
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[3]))
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[4]))
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[5]))
+                    subject.metadata['date']          = iXX[0]
+                    subject.metadata['subject_id']    = iXX[1]
+                    subject.metadata['Filename1']     = iXX[2]
+                    subject.metadata['Filename2']     = iXX[3]
+                    subject.metadata['Filename3']     = iXX[4]
+                    subject.metadata['Filename4']     = iXX[5]
+                    subject.metadata['#ML_Posterior'] = iXX[6]
+                    subject.save()
+                    tmp.append(subject)
+                subjectset.add(tmp)
 
-                if (workflow in [A]):
-                    for iXX in Appre:
-                        subject = Subject()
-                        subject.links.project = project
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[2]))
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[3]))
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[4]))
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[5]))
-                        subject.metadata['date']          = iXX[0]
-                        subject.metadata['subject_id']    = iXX[1]
-                        subject.metadata['Filename1']     = iXX[2]
-                        subject.metadata['Filename2']     = iXX[3]
-                        subject.metadata['Filename3']     = iXX[4]
-                        subject.metadata['Filename4']     = iXX[5]
-                        subject.metadata['#ML_Posterior'] = iXX[6]
-                        subject.save()
-                        tmp.append(subject)
-                    subjectset.add(tmp)
+            if (workflow in [A]):
+                for iXX in Appre:
+                    subject = Subject()
+                    subject.links.project = project
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[2]))
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[3]))
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[4]))
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[5]))
+                    subject.metadata['date']          = iXX[0]
+                    subject.metadata['subject_id']    = iXX[1]
+                    subject.metadata['Filename1']     = iXX[2]
+                    subject.metadata['Filename2']     = iXX[3]
+                    subject.metadata['Filename3']     = iXX[4]
+                    subject.metadata['Filename4']     = iXX[5]
+                    subject.metadata['#ML_Posterior'] = iXX[6]
+                    subject.save()
+                    tmp.append(subject)
+                subjectset.add(tmp)
 
-                if (workflow in [M]):
-                    for iXX in Master:
-                        subject = Subject()
-                        subject.links.project = project
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[2]))
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[3]))
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[4]))
-                        subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[5]))
-                        subject.metadata['date']          = iXX[0]
-                        subject.metadata['subject_id']    = iXX[1]
-                        subject.metadata['Filename1']     = iXX[2]
-                        subject.metadata['Filename2']     = iXX[3]
-                        subject.metadata['Filename3']     = iXX[4]
-                        subject.metadata['Filename4']     = iXX[5]
-                        subject.metadata['#ML_Posterior'] = iXX[6]
-                        subject.save()
-                        tmp.append(subject)
-
-                    subjectset.add(tmp)
-
+            if (workflow in [M]):
+                for iXX in Master:
+                    subject = Subject()
+                    subject.links.project = project
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[2]))
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[3]))
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[4]))
+                    subject.add_location('{0}/{1}/Beginner/{2}'.format(dataPath,Type,iXX[5]))
+                    subject.metadata['date']          = iXX[0]
+                    subject.metadata['subject_id']    = iXX[1]
+                    subject.metadata['Filename1']     = iXX[2]
+                    subject.metadata['Filename2']     = iXX[3]
+                    subject.metadata['Filename3']     = iXX[4]
+                    subject.metadata['Filename4']     = iXX[5]
+                    subject.metadata['#ML_Posterior'] = iXX[6]
+                    subject.save()
+                    tmp.append(subject)
+                subjectset.add(tmp)
     except:
         print("Fail: {0}".format(Type))
-
