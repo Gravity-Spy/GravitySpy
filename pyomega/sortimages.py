1bc1058e75457f9f9df0918b2ffd6130d90ddedc
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2017-02-16 13:15:03 -0600

    Making the upload script more informative

--- pyomega/sortimages.py
+++ pyomega/sortimages.py
@@ -17,6 +17,7 @@ project = Project.find(slug='zooniverse/gravity-spy')
 engine = create_engine('postgresql://scoughlin@localhost:5432/gravityspy')
 triggers = pd.read_sql('glitches',engine)
 triggers = triggers.loc[triggers.UploadFlag == 0]
+print(len(triggers))
 
 labels    = triggers.Label.unique()
 
@@ -27,6 +28,8 @@ for iLabel in labels:
         subjectset = SubjectSet.find(int(iSubjectSet))
         tmp = tmp1.loc[tmp1.subjectset == iSubjectSet]
         subjectsToUpload = []
+        iT = 0
+        IDlist = []
         for index,iSubject in tmp.iterrows():
             subject = Subject()
             subject.links.project = project
@@ -41,10 +44,14 @@ for iLabel in labels:
             subject.metadata['Filename3']     = iSubject['Filename3'].split('/')[-1].split('/')[-1]
             subject.metadata['Filename4']     = iSubject['Filename4'].split('/')[-1].split('/')[-1]
             subject.metadata['#ML_Posterior_20170215'] = str(iSubject[["1080Lines","1400Ripples","Air_Compressor","Blip","Chirp","Extremely_Loud","Helix","Koi_Fish","Light_Modulation","Low_Frequency_Burst","Low_Frequency_Lines","No_Glitch","None_of_the_Above","Paired_Doves","Power_Line","Repeating_Blips","Scattered_Light","Scratchy","Tomte","Violin_Mode","Wandering_Line","Whistle"]].values.tolist())
-
+            IDlist.append(iSubject['uniqueID'])
             subject.save()
             subjectsToUpload.append(subject)
+            iT = iT + 1
+            if iT == 19:
+                break
         subjectset.add(subjectsToUpload)
-        SQLCommand = 'UPDATE glitches SET \"UploadFlag\" = \"UploadFlag\" + 1 WHERE \"Label\" = \'{0}\' AND \"subjectset\" = {1}'.format(iLabel,iSubjectSet)
-        print(time.time() - startTime)
-        engine.execute(SQLCommand)
+        for iID in IDlist:
+            SQLCommand = 'UPDATE glitches SET \"UploadFlag\" = \"UploadFlag\" + 1 WHERE \"uniqueID\" = \'{0}\''.format(iID)
+            engine.execute(SQLCommand)
+        print('Label: {0}, SubjectSet: {1}, Time: {2}, IDs: {3}'.format(iLabel,iSubjectSet,time.time() - startTime,IDlist))
