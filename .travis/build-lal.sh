fbe7924dc45d4487d2d2bfadc1ab11ae6a2559d0
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2017-04-26 09:00:11 -0700

    Updating travis again almost ready to rock and role

--- .travis/build-lal.sh
+++ .travis/build-lal.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# set paths for PKG_CONFIG
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${VIRTUAL_ENV}/lib/pkgconfig
+set -e
+set -x
 
-# build a newer version of swig
-bash -e .travis/build-with-autotools.sh swig-${SWIG_VERSION}/${TRAVIS_PYTHON_VERSION} ${SWIG_}
+LAL_TAG="lal-${LAL_VERSION}"
+LAL_TARBALL="${LAL_TAG}.tar.xz"
+LAL_SOURCE="http://software.ligo.org/lscsoft/source/lalsuite"
 
-# build FFTW3 (double and float)
-bash -e .travis/build-with-autotools.sh fftw-${FFTW_VERSION}/${TRAVIS_PYTHON_VERSION} ${FFTW} --enable-shared=yes
-bash -e .travis/build-with-autotools.sh fftw-${FFTW_VERSION}-float/${TRAVIS_PYTHON_VERSION} ${FFTW} --enable-shared=yes --enable-float
+target=`python -c "import sys; print(sys.prefix)"`
 
+echo "----------------------------------------------------------------------"
+echo "Installing from ${LAL_TARBALL}"
 
-# build LAL packages
-bash -e .travis/build-with-autotools.sh lal-${LAL_VERSION}/${TRAVIS_PYTHON_VERSION} ${LAL} --enable-swig-python
+wget ${LAL_SOURCE}/${LAL_TARBALL} -O ${LAL_TARBALL} --quiet
+mkdir -p ${LAL_TAG}
+tar -xf ${LAL_TARBALL} --strip-components=1 -C ${LAL_TAG}
+cd ${LAL_TAG}
+./configure --enable-silent-rules --enable-swig-python --quiet --prefix=${target}
+make --silent
+make install --silent
