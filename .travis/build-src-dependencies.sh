a934cec3e5f3fed0f73f13a26835a09f5e400601
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2017-04-26 07:41:10 -0700

    Taking Duncan travis.yml as basis for GravitySpy nightly builds

--- .travis/build-src-dependencies.sh
+++ .travis/build-src-dependencies.sh
@@ -3,6 +3,10 @@
 # set paths for PKG_CONFIG
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${VIRTUAL_ENV}/lib/pkgconfig
 
+# use g++ 4.7 (mainly for framecpp)
+export CXX=`which g++-4.7`
+echo "Set CXX=${CXX}"
+
 # get latest version of packages
 . .travis/find-latest-releases.sh
 
@@ -16,6 +20,16 @@ bash -e .travis/build-with-autotools.sh \
 
 FAILURES=""
 
+# -- framecpp -----------------------------------------------------------------
+
+bash .travis/build-with-autotools.sh \
+    python-${TRAVIS_PYTHON_VERSION}-ldas-tools-al \
+    ${LDAS_TOOLS_AL} --enable-python || FAILURES="$FAILURES ldas-tools-al"
+
+bash .travis/build-with-autotools.sh \
+    python-${TRAVIS_PYTHON_VERSION}-framecpp \
+    ${FRAMECPP} --enable-python || FAILURES="$FAILURES framecpp"
+
 # -- lalframe -----------------------------------------------------------------
 
 bash .travis/build-with-autotools.sh \
@@ -26,7 +40,7 @@ bash .travis/build-with-autotools.sh \
 
 bash .travis/build-with-autotools.sh \
     python-${TRAVIS_PYTHON_VERSION}-nds2-client \
-    ${NDS2_CLIENT} --disable-swig-java --disable-mex-matlab || FAILURES="$FAILURES nds2-client"
+    ${NDS2_CLIENT} -DENABLE_SWIG_JAVA=false -DENABLE_SWIG_MATLAB=false || FAILURES="$FAILURES nds2-client"
 
 # -- exit ---------------------------------------------------------------------
 
