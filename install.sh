f11f8bc0f36e68e36ad134bff89a8f5c3d56a081
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2017-04-26 06:38:04 -0700

    Fixing mistake

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-mkdir ~/python      
-cd ~/python
+mkdir ~/Project/python
+cd ~/Project/python
 wget https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
 tar zxfv Python-2.7.13.tgz
 cd Python-2.7.13
 
-./configure --prefix=$HOME/python
+./configure --prefix=$HOME/Project/python
 make && make altinstall
