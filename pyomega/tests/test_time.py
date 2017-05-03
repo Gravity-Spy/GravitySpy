6f47ad9769e098b302880ce0cd532f0cb3888c2d
Scott Coughlin <scott.coughlin@ligo.org>
2017-05-03 15:39:36 -0700

    Finally a unit test that works

--- pyomega/tests/test_time.py
+++ pyomega/tests/test_time.py
@@ -9,21 +9,20 @@ import pyomega.ML.make_pickle_for_linux as make_pickle
 import pyomega.ML.labelling_test_glitches as label_glitches
 TEST_IMAGES_PATH = os.path.join(os.path.split(__file__)[0], 'data',
 'images')
-print(TEST_IMAGES_PATH)
 
-Score = 0.964064
+Score = 0.96406441927 
 
 class GravitSpyTests(unittest2.TestCase):
     """`TestCase` for the GravitySpy
     """
     def test_pickle(self):
-        make_pickle.main(TEST_IMAGES_PATH + '/folder1/folder2/',
+        make_pickle.main(TEST_IMAGES_PATH + '/folder1/',
                          TEST_IMAGES_PATH + '/folder1/folder2/pickleddata/', 1, 0)
         scores, MLlabel = label_glitches.main(TEST_IMAGES_PATH + '/folder1/folder2/pickleddata/',
-                                              '../pyomega/ML/trained_model/'.format(pathToModel),
+                                              '../ML/trained_model/',
                                               TEST_IMAGES_PATH + '/folder1/folder2/labeled/',
                                               0)
-        self.assertEqual(scores[MLlabel], Score)
+        self.assertEqual(float(scores[MLlabel + 1]), Score)
 
 
 if __name__ == '__main__':
