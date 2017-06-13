5453dabf212b4d4835f58e30278ce2f1c6cab855
Scott Coughlin <scott.coughlin@ligo.org>
2017-06-13 09:11:32 -0700

    Already ready to get up tod ate with current triggers

--- PreProcessing/O2.sh
+++ PreProcessing/O2.sh
@@ -2,4 +2,6 @@
 
 source ~/.bash_profile
 
-gettriggers --outDir /home/scoughlin/public_html/O2/GravitySpy/Format/Feb152017/images/ML/ --uniqueID --SNR 7.5 --pathToIni /home/scoughlin/O2/automated/GravitySpy//pyomega/Production/wini.ini --pathToModel /home/scoughlin/O2/automated/GravitySpy//pyomega/src/ML/trained_model/ --PostgreSQL --upload 
+cd ~/Project/GravitySpy/PreProcessing
+
+gettriggers --outDir /home/scoughlin/public_html/O2/GravitySpy/Format/Feb152017/images/ML/ --uniqueID --SNR 7.5 --pathToIni /home/scoughlin/O2/automated/GravitySpy//pyomega/Production/wini.ini --pathToModel /home/scoughlin/Project/GravitySpy/pyomega/ML/trained_model/ --PostgreSQL --upload 
