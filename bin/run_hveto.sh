30a69e207ef4c008bffa41946fef864a8086343e
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2017-06-22 08:41:33 -0500

    Fatal Error in gettriggers

--- bin/run_hveto.sh
+++ bin/run_hveto.sh
@@ -7,9 +7,9 @@ path=`pwd`
 
 dur=$(expr ${gpsEnd} - ${gpsStart})
 
-./filter_psql -d $ifo -l $label -o $path/$ifo-triggers-${gpsStart}-${dur}.csv -f segs_${gpsStart}_${gpsEnd}.dat -p 1 -s $gpsStart -e $gpsEnd
+filter_psql -d $ifo -l $label -o $path/$ifo-triggers-${gpsStart}-${dur}.csv -f segs_${gpsStart}_${gpsEnd}.dat -p 1 -s $gpsStart -e $gpsEnd --database O1GlitchClassificationUpdate
 
 ls $path/$ifo-triggers-${gpsStart}-${dur}.csv | lalapps_path2cache > cache.lcf
 
-hveto $gpsStart $gpsEnd --ifo $ifo --config-file h1l1-hveto-daily-o2.ini -p cache.lcf -o ~/public_html/HVeto/$ifo/$label/$gpsStart-$gpsEnd --nproc 4 --omega-scans 5
+hveto $gpsStart $gpsEnd --ifo $ifo --config-file h1l1-hveto-daily-o2.ini -p cache.lcf -o ~/public_html/HVeto/$ifo/$label/$gpsStart-$gpsEnd --nproc 32 --omega-scans 5
 
