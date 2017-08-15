37d91a034064c4d5fd489115c08bc41e2653f67a
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2017-08-15 11:52:54 +0100

    Almost appeased PEP

--- docs/conf.py
+++ docs/conf.py
@@ -140,7 +140,7 @@ html_theme = 'sphinx_rtd_theme'
 # html_logo = None
 
 # The name of an image file (relative to this directory) to use as a favicon of
-# the docs.  This file should be a Windows icon file (.ico) being 16x16 
+# the docs.  This file should be a Windows icon file (.ico) being 16x16
 # or 32x32 pixels large.
 # html_favicon = None
 
