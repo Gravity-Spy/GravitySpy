981b859bede2b5036990fffa2a87607b2360f1d1
Michael Zevin <michael.zevin@ligo.org>
2018-01-23 13:58:06 +0000

    function for adding user weights to classification table

--- API/weight_users.py
+++ API/weight_users.py
@@ -8,12 +8,8 @@ import pdb
 from pyomega.API import projectStructure
 from pyomega.API import calcConfMatrix
 
-# Obtain number of classes from API
-workflowDictSubjectSets = projectStructure.main('1104','O2')
-classes = sorted(workflowDictSubjectSets[2117].keys())
-
-# From ansers Dict determine number of classes
-numClasses = len(classes)
+# List of defunct workflows
+defunct_workflows = [1479, 3063, 5106]
 
 # Load info about classifications and glitches
 print '\nreading classifications...'
@@ -28,7 +24,7 @@ users = classifications.links_user.unique()
 
 # Cycle through users, and append weight for each classification
 # NOTE: B1=1610, B2=1934, B3=1935, A=2360, M=2117
-for user in users:
+for idx, user in enumerate(users):
     # sort classifications by date
     user_classifications = classifications[classifications.links_user == user]
     user_classifications = user_classifications.sort_values(by=['metadata_finished_at'])
@@ -38,6 +34,7 @@ for user in users:
     if master_index.size != 0:
         master_index = master_index[0]
         master_index = user_classifications[user_classifications.metadata_finished_at >= user_classifications.loc[master_index, 'metadata_finished_at']].index
+
     # find the indices at which they are an apprentice, or set to -1
     apprentice_index = user_classifications[user_classifications.links_workflow == 2360].index
     if apprentice_index.size != 0:
@@ -49,8 +46,14 @@ for user in users:
         classifications.loc[apprentice_index, 'weight'] = 1.0
     if master_index.size != 0:
         classifications.loc[master_index, 'weight'] = 2.0
-    
-    print user
 
-classifications.to_pickle('weighted_classifications.pkl')
+    # see how far into the simulation we are
+    if idx%100 == 0:
+        sys.stderr.write('\r {:04.2f}% complete'.format(100*float(idx)/len(users)))
+
+# weight all defunct classes with a weight of 0.0
+classifications.loc[classifications.links_workflow.isin(defunct_workflows), 'weight'] = 0.0
+
+print '\nwriting to pickle...'
+classifications.to_pickle('pickled_data/weighted_classifications.pkl')
 
