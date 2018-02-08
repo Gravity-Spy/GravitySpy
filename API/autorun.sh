c288c052afaf3d4f8f19af84083d97a2a552ca78
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-02-08 11:01:31 -0600

    Moving all Zoo API python functions into executable in the bin folder where they definitely belong

--- API/autorun.sh
+++
@@ -1,4 +0,0 @@
-/home/sbc538/software/GSpy/bin/python callAPI.py
-/home/sbc538/software/GSpy/bin/python CC.py
-/home/sbc538/software/GSpy/bin/python CCPostProc.py
-/home/sbc538/software/GSpy/bin/python DataProducts.py  --outPath /var/www/html/images/DataProducts/GravitySpySummary/ --pp-matrix /var/www/html/images/DataProducts/pp_matrices --confusion-matrix /var/www/html/images/DataProducts/confusion
