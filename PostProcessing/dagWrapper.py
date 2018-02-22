98d3402e8c89cf95860c27ea10b90b7362faa148
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-02-22 09:38:45 -0600

    If I am going to do a v0.1 release then I need to clean thi directory and get all of the code unit tested

--- PostProcessing/dagWrapper.py
+++
@@ -1,6 +0,0 @@
-tmp = open('uploadImage.dag','w')
-iT=0
-for iLabel in ["1080Lines","1400Ripples","Air_Compressor","Blip","Chirp","Extremely_Loud","Helix","Koi_Fish","Light_Modulation","Low_Frequency_Burst","Low_Frequency_Lines","No_Glitch","None_of_the_Above","Paired_Doves","Power_Line","Repeating_Blips","Scattered_Light","Scratchy","Tomte","Violin_Mode","Wandering_Line","Whistle"]:
-    iT= iT +1
-    tmp.write('JOB {0} ./condor/gravityspy.sub\n'.format(iT))
-    tmp.write('VARS {0} jobNumber="{0}" Label="{1}"\n'.format(iT,iLabel))
