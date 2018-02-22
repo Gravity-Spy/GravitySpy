58246c2d8185267f69a081ac207ebdda013ad356
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-02-22 09:47:52 -0600

    Updating the environmental variable name to be consitent with that expected by gwpy when using Gravity Spy

--- pyomega/API/calcConfMatrix.py
+++ pyomega/API/calcConfMatrix.py
@@ -17,7 +17,7 @@ from scipy.sparse import coo_matrix
 
 def main():
 
-    engine = create_engine('postgresql://{0}:{1}@gravityspy.ciera.northwestern.edu:5432/gravityspy'.format(os.environ['QUEST_SQL_USER'],os.environ['QUEST_SQL_PASSWORD']))
+    engine = create_engine('postgresql://{0}:{1}@gravityspy.ciera.northwestern.edu:5432/gravityspy'.format(os.environ['GRAVITYSPY_DATABASE_USER'],os.environ['QUEST_SQL_PASSWORD']))
 
     # Load classifications, current user DB status and golden images from DB
     classifications = pd.read_sql('SELECT links_user, links_subjects, links_workflow, "annotations_value_choiceINT" FROM classificationsdev', engine)
