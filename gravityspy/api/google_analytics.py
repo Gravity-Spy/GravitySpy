e33b2ac390d03da92cfe908a73e9c9de18085241
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2019-11-19 12:41:53 -0600

    Minor update

--- gravityspy/api/google_analytics.py
+++ gravityspy/api/google_analytics.py
@@ -7,14 +7,16 @@ import httplib2
 import datetime
 
 from apiclient.discovery import build
+import httplib2
 from oauth2client import client
 from oauth2client import file
 from oauth2client import tools
 
+
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
-DISCOVERY_URI = ('https://analyticsreporting.googleapis.com/$discovery/rest')
-CLIENT_SECRETS_PATH = 'client_secrets.json' # Path to client_secrets.json file.
+CLIENT_SECRETS_PATH = 'client_secrets.json'
 VIEW_ID = '97262563'
+
 
 def initialize_analyticsreporting():
     """Initializes the analyticsreporting service object.
@@ -24,14 +26,14 @@ def initialize_analyticsreporting():
     """
     # Parse command-line arguments.
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        parents=[tools.argparser])
+      formatter_class=argparse.RawDescriptionHelpFormatter,
+      parents=[tools.argparser])
     flags = parser.parse_args([])
 
     # Set up a Flow object to be used if we need to authenticate.
     flow = client.flow_from_clientsecrets(
-        CLIENT_SECRETS_PATH, scope=SCOPES,
-        message=tools.message_if_missing(CLIENT_SECRETS_PATH))
+      CLIENT_SECRETS_PATH, scope=SCOPES,
+      message=tools.message_if_missing(CLIENT_SECRETS_PATH))
 
     # Prepare credentials, and authorize HTTP object with them.
     # If the credentials don't exist or are invalid run through the native client
@@ -39,14 +41,12 @@ def initialize_analyticsreporting():
     # credentials will get written back to a file.
     storage = file.Storage('analyticsreporting.dat')
     credentials = storage.get()
-
     if credentials is None or credentials.invalid:
         credentials = tools.run_flow(flow, storage, flags)
-
     http = credentials.authorize(http=httplib2.Http())
 
     # Build the service object.
-    analytics = build('analytics', 'v4', http=http, discoveryServiceUrl=DISCOVERY_URI)
+    analytics = build('analyticsreporting', 'v4', http=http)
 
     return analytics
 
