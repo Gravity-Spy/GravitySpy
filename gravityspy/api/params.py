a4063e1b9006493892365bd74d8a655de38c4cf4
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2021-02-05 14:05:23 -0800

    So we are close but something about the posteriors of people who never classified a given glitch befor elooking at it is still a problem.

--- gravityspy/api/params.py
+++ gravityspy/api/params.py
@@ -22,8 +22,8 @@ class Weighting:
         we read in the classificaitons in the main function already, so these will just be arguments to ther pertinenet weighting schemes
         '''
         # make dict for relating workflow to weighting
-        workflow_dict = {'1610': 'B1', '1934': 'B2', '1935': 'B3', '2360': 'A', '2117': 'M'}
-        b05_a1_m2_dict = {'B1': 0.5, 'B2': 0.5, 'B3': 0.5, 'A': 1.0, 'M': 2.0}
+        workflow_dict = {'1610': 'B1', '1934': 'B2', '1935': 'B3', '7765': 'B4', '7766': 'A', '7767': 'M'}
+        b05_a1_m2_dict = {'B1': 0.5, 'B2': 0.5, 'B3': 0.5, 'B4' : 0.5, 'A': 1.0, 'M': 2.0}
 
     def default(self, data, user, glitch):
         """
@@ -43,12 +43,12 @@ class Weighting:
         # find the ID of the glitch classification in question (should only be 1 classification)
         classificationNum=np.argwhere(userClassifications.links_subjects == glitch).min()
         # find IDs of apprentice and master workflow classificaitons
-        apprenticeClass = np.argwhere(userClassifications.links_workflow == 2360)
+        apprenticeClass = np.argwhere(userClassifications.links_workflow == 7766)
         if len(apprenticeClass) > 0:
             apprenticeNum = apprenticeClass.min()
         else:
             apprenticeNum = -1
-        masterClass = np.argwhere(userClassifications.links_workflow == 2117)
+        masterClass = np.argwhere(userClassifications.links_workflow == 7767)
         if len(masterClass) > 0:
             masterNum = masterClass.min()
         else:
@@ -81,12 +81,12 @@ class Weighting:
         # find the ID of the glitch classification in question (should only be 1 classification)
         classificationNum=np.argwhere(userClassifications.links_subjects == glitch).min()
         # find IDs of apprentice and master workflow classificaitons
-        apprenticeClass = np.argwhere(userClassifications.links_workflow == 2360)
+        apprenticeClass = np.argwhere(userClassifications.links_workflow == 7766)
         if len(apprenticeClass) > 0:
             apprenticeNum = apprenticeClass.min()
         else:
             apprenticeNum = -1
-        masterClass = np.argwhere(userClassifications.links_workflow == 2117)
+        masterClass = np.argwhere(userClassifications.links_workflow == 7767)
         if len(masterClass) > 0:
             masterNum = masterClass.min()
         else:
