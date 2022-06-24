647cce0a6fbbb405cd766ba18cb879c46e0456e8
siddharth soni <siddharth.soni@jupyter.ligo-wa.caltech.edu>
2022-06-24 11:20:57 -0700

    np.int to int, multichannel to channel_axis

--- gravityspy/ml/read_image.py
+++ gravityspy/ml/read_image.py
@@ -56,9 +56,9 @@ def read_grayscale(filename, resolution=0.3, x=[66, 532], y=[105, 671],
 
     image_data = rgb2gray(image_data)
     image_data = rescale(image_data, resolution, mode='constant',
-                         preserve_range='True', multichannel=False)
+                         preserve_range='True', channel_axis=None)
 
-    dim = np.int(reduce(lambda x, y: x * y, image_data.shape))
+    dim = int(reduce(lambda x, y: x * y, image_data.shape))
     image_data = np.reshape(image_data, (dim))
     image_data = np.array(image_data, dtype='f')
 
@@ -86,7 +86,7 @@ def read_rgb(filename, resolution=0.3, x=[66, 532], y=[105, 671],
     image_data = read_and_crop_image(filename, x=x, y=y)
     image_data = rescale(image_data, resolution, mode='constant',
                          preserve_range='True', multichannel=True)
-    dim = np.int(reduce(lambda x, y: x * y, image_data[:,:,0].shape))
+    dim = int(reduce(lambda x, y: x * y, image_data[:,:,0].shape))
     image_data_r = np.reshape(image_data[:,:,0], (dim))
     image_data_g = np.reshape(image_data[:,:,1], (dim))
     image_data_b = np.reshape(image_data[:,:,2], (dim))
