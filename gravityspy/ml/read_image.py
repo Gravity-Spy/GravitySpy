dc198d5f0b05bf363002456db4dd7aa13b5359d9
Siddharth Soni <siddharthsoni22@gmail.com>
2022-06-25 15:05:30 -0400

    pep8 formatting

--- gravityspy/ml/read_image.py
+++ gravityspy/ml/read_image.py
@@ -2,7 +2,6 @@ from skimage import io
 from skimage.color import rgb2gray
 from skimage.transform import rescale
 import numpy as np
-import os
 from functools import reduce
 
 
@@ -32,6 +31,7 @@ def read_and_crop_image(filename, x, y):
     image_data = io.imread(filename)
     image_data = image_data[xmin:xmax, ymin:ymax, :3]
     return image_data
+
 
 def read_grayscale(filename, resolution=0.3, x=[66, 532], y=[105, 671],
                    verbose=False):
@@ -64,6 +64,7 @@ def read_grayscale(filename, resolution=0.3, x=[66, 532], y=[105, 671],
 
     return image_data
 
+
 def read_rgb(filename, resolution=0.3, x=[66, 532], y=[105, 671],
              verbose=False):
     """Convert image from RGB to Gray, downsample
@@ -86,9 +87,9 @@ def read_rgb(filename, resolution=0.3, x=[66, 532], y=[105, 671],
     image_data = read_and_crop_image(filename, x=x, y=y)
     image_data = rescale(image_data, resolution, mode='constant',
                          preserve_range='True', multichannel=True)
-    dim = int(reduce(lambda x, y: x * y, image_data[:,:,0].shape))
-    image_data_r = np.reshape(image_data[:,:,0], (dim))
-    image_data_g = np.reshape(image_data[:,:,1], (dim))
-    image_data_b = np.reshape(image_data[:,:,2], (dim))
+    dim = int(reduce(lambda x, y: x * y, image_data[:, :, 0].shape))
+    image_data_r = np.reshape(image_data[:, :, 0], (dim))
+    image_data_g = np.reshape(image_data[:, :, 1], (dim))
+    image_data_b = np.reshape(image_data[:, :, 2], (dim))
 
     return image_data_r, image_data_g, image_data_b
